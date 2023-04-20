a5a75c507739a81c1ff9d1c8c2596c474b597d41
Daw <Daw@DESKTOP-8OVFM6T>
2023-04-20 17:35:00 +0200

    Cambios realizados con éxtiso

---
+++ Proyecto1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Daw
+ *
+ */
+module Proyecto1 {
+}
---
+++ Proyecto1/src/paquete1/Principal.java
@@ -0,0 +1,10 @@
+package paquete1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Estamos haciendo control de versiones con Git");
+	}
+
+}
