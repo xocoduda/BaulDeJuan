34dd7e093fe2430acf3edb33aa225612c8e9dc76
Daw <Daw@DESKTOP-8OVFM6T>
2023-04-20 17:38:26 +0200

    Cambios OK

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Nuevo mensaje el en cuadro GIT");
+		System.out.println("Última prueba en el main");
 	}
 
 }
