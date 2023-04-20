b305496010f705b9129fe4134c36bff9e6ac8125
Daw <Daw@DESKTOP-8OVFM6T>
2023-04-20 17:40:05 +0200

    Cambios hechos

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -5,6 +5,8 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Última prueba en el main");
+		System.out.println("Última prueba en el main2");
+		System.out.println("Última prueba en el main3");
 	}
 
 }
