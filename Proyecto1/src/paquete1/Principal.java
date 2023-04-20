fb6e95d8416e75fcdabdc70b91c7adb1b3de3f6f
Daw <Daw@DESKTOP-8OVFM6T>
2023-04-20 17:36:00 +0200

    Cambios realizados

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Estamos haciendo control de versiones con Git");
+		System.out.println("Nuevo mensaje el en cuadro GIT");
 	}
 
 }
